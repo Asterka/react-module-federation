6151810205927ca9505b9f68e2a9a42f154eebc7
Аскар Хабибуллин <a.khabibullin@innopolis.ru>
2021-11-12 11:24:56 +0300

    Init

---
+++ dist/506.js
@@ -0,0 +1,1 @@
+(self.webpackChunkmy_webpack_project=self.webpackChunkmy_webpack_project||[]).push([[506,179],{506:function(e,o,p){Promise.all([p.e(935),p.e(451)]).then(p.bind(p,451)),console.log("I am bootsrtapped")}}]);
---
+++ src/index.js
@@ -0,0 +1,3 @@
+
+import('./bootstrap');
+console.log("I am bootsrtapped");
